a75356d57420ee18ec4d6a1f20fce25564588d66
Daniel Robert McManus <dm480673@southeast.edu>
2020-03-09 14:47:43 -0500

    Made layout work

--- TOME/home/views.py
+++ TOME/home/views.py
@@ -4,7 +4,13 @@ import re
 from datetime import datetime
 
 def home(request):
-    return HttpResponse("Hello, Django!")
+    return render(request, "home/home.html")
+
+def about(request):
+    return render(request, "home/about.html")
+
+def contact(request):
+    return render(request, "home/contact.html")
 
 def hello_there(request, name):
     return render(
